0e1e407c625a75bdb942a6bb4b4ad69ae7544a9c
mcamaya <camiamayar@gmail.com>
2023-08-29 13:22:32 -0500

    feat: :sparkles: Recibiendo parámetros en la request

--- index.js
+++ index.js
@@ -11,13 +11,46 @@ app.get('/nueva-ruta', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
+  res.json([
+    {
+      route: 'Ruta productos',
+      name: 'My Product',
+      price: 100000,
+      category: 'My Category'
+    },
+    {
+      route: 'Ruta productos',
+      name: 'My Product',
+      price: 100000,
+      category: 'My Category'
+    },
+    {
+      route: 'Ruta productos',
+      name: 'My Product',
+      price: 100000,
+      category: 'My Category'
+    }
+  ]); // res.json para crear una API
+});
+
+app.get('/products/:id', (req, res) => {
+  const {id} = req.params;
   res.json({
+    id,
     route: 'Ruta productos',
     name: 'My Product',
     price: 100000,
     category: 'My Category'
-  }); // res.json para crear una API
+  });
 });
+
+app.get('/categories/:ctgId/products/:prdId', (req, res) => {
+  const {ctgId, prdId} = req.params;
+  res.json({
+    ctgId,
+    prdId
+  })
+})
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
