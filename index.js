27be6e81eae19d968b214b9e62eeeba3c96b7087
mcamaya <camiamayar@gmail.com>
2023-08-29 07:28:26 -0500

    feat: :sparkles: Routing desde app - res.send & res.json

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('my app');
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, esta es una nueva ruta');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    route: 'Ruta productos',
+    name: 'My Product',
+    price: 100000,
+    category: 'My Category'
+  }); // res.json para crear una API
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
