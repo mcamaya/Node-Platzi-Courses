8a7a799ca7c7c37cc1d290dd2480d6e6479faad4
mcamaya <camiamayar@gmail.com>
2023-08-30 06:46:09 -0500

    feat: :sparkles: Ruta padre o path global para todos los endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
+const { Router } = require('express');
 const productsRouter = require('./products.routes.js');
 const usersRouter = require('./users.routes.js');
 const categoriesRouter = require('./categories.routes.js');
 
 function routersApi(app){
-  app.use('/products', productsRouter);
-  app.use('/users', usersRouter);
-  app.use('/categories', categoriesRouter);
+  const router = Router();
+  app.use('/api/v1', router)
+  router.use('/products', productsRouter);
+  router.use('/users', usersRouter);
+  router.use('/categories', categoriesRouter);
 }
 
 module.exports = routersApi;
